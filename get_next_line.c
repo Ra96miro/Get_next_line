20960c0eeaa2e03693771fb5496aa509aa170509
Ra96miro <100141204+Ra96miro@users.noreply.github.com>
2022-04-04 19:31:57 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,20 @@
 /*   By: gramiro- <gramiro-@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 03:25:58 by gramiro-          #+#    #+#             */
-/*   Updated: 2022/04/01 12:45:29 by gramiro-         ###   ########.fr       */
+/*   Updated: 2022/04/04 19:29:58 by gramiro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-char	*get_save(char *buff)
+void	get_free(char *buff, char *file)
+{
+	free(file);
+	free(buff);
+}
+
+char	*get_save(char *save)
 {
 	int		x;
 	int		y;
@@ -21,40 +27,41 @@ char	*get_save(char *buff)
 
 	x = 0;
 	y = 0;
-	while (buff[x] != '\n' && buff[x])
+	while (save[x] != '\n' && save[x])
 		x++;
-	rest = (char *)malloc(sizeof(char) * (ft_strlen(buff) - x + 1));
+	if (!save[x])
+	{
+		free(save);
+		return (0);
+	}
+	rest = (char *)malloc(sizeof(char) * (ft_strlen(save) - x + 1));
 	if (!rest)
 		return (0);
 	x++;
-	while (buff[x])
-	{
-		rest[y] = buff[x];
-		x++;
-		y++;
-	}
+	while (save[x])
+		rest[y++] = save[x++];
 	rest[y] = '\0';
-	free(buff);
+	free(save);
 	return (rest);
 }
 
-char	*get_line(char *buff)
+char	*get_line(char *save)
 {
 	int		x;
 	char	*s;
 
 	x = 0;
-	if (!buff)
+	if (!save)
 		return (0);
-	while (buff[x] != '\n' && buff[x])
+	while (save[x] != '\n' && save[x])
 		x++;
 	s = (char *)malloc(sizeof(char) * (x + 2));
 	if (!s)
 		return (0);
 	x = -1;
-	while (buff[++x] != '\n' && buff[x])
-		s[x] = buff[x];
-	if (buff[x] == '\n')
+	while (save[++x] != '\n' && save[x])
+		s[x] = save[x];
+	if (save[x] == '\n')
 	{
 		s[x] = '\n';
 		s[x + 1] = '\0';
@@ -73,13 +80,21 @@ char	*get_read_file(int fd, char *file)
 	if (!buff)
 		return (0);
 	size = 1;
-	while (!ft_strchr(buff, '\n') && size != 0)
+	while (!ft_strchr(file, '\n') && size != 0)
 	{
 		size = read(fd, buff, BUFFER_SIZE);
 		if (size == -1)
+		{
+			free(buff);
 			return (0);
+		}
 		buff[size] = '\0';
 		file = ft_strjoin(file, buff);
+	}
+	if (ft_strlen(file) <= 0)
+	{
+		get_free(buff, file);
+		return (0);
 	}
 	free(buff);
 	return (file);
@@ -93,6 +108,8 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	save = get_read_file(fd, save);
+	if (!save)
+		return (0);
 	line = get_line(save);
 	save = get_save(save);
 	return (line);
